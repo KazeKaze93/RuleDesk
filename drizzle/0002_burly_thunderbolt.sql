ce4bacc2f5a85b274a64b1cd65ffcd4f530c44ff
Kakha-Khinikadze <kvintas93@gmail.com>
2025-12-13 06:38:28 +0400

    feat: add new journal entry for burly thunderbolt
    - Introduced a new journal entry with index 2, version 5, and associated metadata for tracking purposes.

---
+++ drizzle/0002_burly_thunderbolt.sql
@@ -0,0 +1,36 @@
+DROP TABLE `subscriptions`;--> statement-breakpoint
+ALTER TABLE `settings` RENAME COLUMN `userId` TO `user_id`;--> statement-breakpoint
+ALTER TABLE `settings` RENAME COLUMN `apiKey` TO `api_key`;--> statement-breakpoint
+DROP INDEX IF EXISTS `artists_tag_unique`;--> statement-breakpoint
+/*
+ SQLite does not support "Drop autoincrement from a column" out of the box, we do not generate automatic migration for that, so it has to be done manually
+ Please refer to: https://www.techonthenet.com/sqlite/tables/alter_table.php
+                  https://www.sqlite.org/lang_altertable.html
+                  https://stackoverflow.com/questions/2083543/modify-a-columns-type-in-sqlite3
+
+ Due to that we don't generate migration automatically and it has to be done manually
+*/--> statement-breakpoint
+/*
+ SQLite does not support "Drop default from column" out of the box, we do not generate automatic migration for that, so it has to be done manually
+ Please refer to: https://www.techonthenet.com/sqlite/tables/alter_table.php
+                  https://www.sqlite.org/lang_altertable.html
+                  https://stackoverflow.com/questions/2083543/modify-a-columns-type-in-sqlite3
+
+ Due to that we don't generate migration automatically and it has to be done manually
+*/--> statement-breakpoint
+/*
+ SQLite does not support "Drop not null from column" out of the box, we do not generate automatic migration for that, so it has to be done manually
+ Please refer to: https://www.techonthenet.com/sqlite/tables/alter_table.php
+                  https://www.sqlite.org/lang_altertable.html
+                  https://stackoverflow.com/questions/2083543/modify-a-columns-type-in-sqlite3
+
+ Due to that we don't generate migration automatically and it has to be done manually
+*/--> statement-breakpoint
+/*
+ SQLite does not support "Set not null to column" out of the box, we do not generate automatic migration for that, so it has to be done manually
+ Please refer to: https://www.techonthenet.com/sqlite/tables/alter_table.php
+                  https://www.sqlite.org/lang_altertable.html
+                  https://stackoverflow.com/questions/2083543/modify-a-columns-type-in-sqlite3
+
+ Due to that we don't generate migration automatically and it has to be done manually
+*/
